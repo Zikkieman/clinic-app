262ab213ebcb2ed5fdac94a581c7e8e0fe971e59
Zikkieman <horlarmeydeileh50@gmail.com>
2023-12-06 13:27:50 +0100

    All users can now get email

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import EmailTemplate from "../../../../email/schedule";
+import sgMail from "@sendgrid/mail";
 
+// import { Resend } from "resend";
+// import EmailTemplate from "../../../../email/schedule";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+const API_KEY = process.env.SEND_MAIL || "";
 
 export async function POST(request: Request) {
   const body = await request.json();
@@ -18,23 +21,29 @@ export async function POST(request: Request) {
     });
   }
 
- try {
-    await resend.emails.send({
-      from: "Healthy Clinic <onboarding@resend.dev>",
-      // to: [email],
-      to: "horlarmeydeileh50@gmail.com",
-      reply_to: "horlarmeydeileh50@gmail.com",
-      subject: "Message from clinic app",
-      react: EmailTemplate({
-        name: name,
-        fullname: fullname,
-        time: time,
-      }),
-    });
+  sgMail.setApiKey(API_KEY);
+  const message = {
+    to: email,
+    from: {
+      name: "Avance Insight team",
+      email: "goldenimperialswifttech@gmail.com",
+    },
+    text: "You recieved the following message from Healthy Clinic",
+    subject: "New message From Healthy Clinic",
+    html: `
+            <p> Dear ${fullname}, We would like to inform you that your scheduled
+            appointment with Dr. ${name} has been confirmed for the specified
+            time "${time}". We kindly request your punctual arrival.</p>
+            `,
+  };
 
-    return NextResponse.json({
-      message: "Appointment Confirmed",
-    });
+  try {
+    const response = await sgMail.send(message);
+    if (response[0].statusCode === 202) {
+      return NextResponse.json({
+        message: "Appointment Confirmed",
+      });
+    }
   } catch (error) {
     return NextResponse.json(error);
   }
